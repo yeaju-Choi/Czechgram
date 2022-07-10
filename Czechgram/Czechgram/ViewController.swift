dd854b6d0af19337c59d547d666b456198f7dfe5
asqw887 <asqw887@gmail.com>
2022-07-10 16:36:13 +0900

    Chore: 프로젝트 초기 셋팅

---
+++ Czechgram/Czechgram/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Czechgram
+//
+//  Created by 최예주 on 2022/07/10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
