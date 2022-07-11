d71cb26648905fb0038ce7e98aad8762beb09673
wnsxor1993 <wnsxor1993@naver.com>
2022-07-11 15:03:45 +0900

    Feat: CollectionView, Cell 구현

--- Czechgram/Czechgram/ViewController.swift
+++ Czechgram/Czechgram/ViewController.swift
@@ -27,6 +27,19 @@ class ViewController: UIViewController {
         return section
     }()
 
+    private let imageSection: UICollectionView = {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout)
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .white
+        collectionView.isPagingEnabled = true
+        collectionView.register(DetailCollectionViewCell.self, forCellWithReuseIdentifier: DetailCollectionViewCell.cellID)
+
+        return collectionView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
@@ -41,6 +54,7 @@ private extension ViewController {
     func setSubViews() {
         view.addSubview(detailScrollView)
         detailScrollView.addSubview(profileSection)
+        detailScrollView.addSubview(imageSection)
     }
 
     func setConstraints() {
@@ -54,11 +68,26 @@ private extension ViewController {
             profileSection.leadingAnchor.constraint(equalTo: detailScrollView.leadingAnchor),
             profileSection.trailingAnchor.constraint(equalTo: detailScrollView.trailingAnchor),
             profileSection.widthAnchor.constraint(equalTo: detailScrollView.widthAnchor),
-            profileSection.heightAnchor.constraint(equalTo: detailScrollView.heightAnchor, multiplier: 0.1)
+            profileSection.heightAnchor.constraint(equalTo: detailScrollView.heightAnchor, multiplier: 0.1),
+
+            imageSection.topAnchor.constraint(equalTo: profileSection.bottomAnchor),
+            imageSection.leadingAnchor.constraint(equalTo: detailScrollView.leadingAnchor),
+            imageSection.trailingAnchor.constraint(equalTo: detailScrollView.trailingAnchor),
+            imageSection.heightAnchor.constraint(equalTo: detailScrollView.heightAnchor, multiplier: 0.6)
         ])
     }
 
     func setSectionsData() {
         profileSection.configureProfile(image: UIImage(), id: "zeto_h_jt")
+    }
+
+    func setCollectionView() {
+        imageSection.delegate = self
+    }
+}
+
+extension ViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        // 추가 구현 예정
     }
 }
