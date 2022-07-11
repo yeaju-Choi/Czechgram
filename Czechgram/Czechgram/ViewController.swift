3b857462e41121e1cff4fda2054dc8e4195c5622
wnsxor1993 <wnsxor1993@naver.com>
2022-07-11 11:09:48 +0900

    Feat: ScrollView add 및 frame autoLayout 설정

--- Czechgram/Czechgram/ViewController.swift
+++ Czechgram/Czechgram/ViewController.swift
@@ -9,8 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private let detailScrollView: UIScrollView = {
+        let scrollView = UIScrollView()
+        scrollView.translatesAutoresizingMaskIntoConstraints = false
+        scrollView.backgroundColor = .white
+        scrollView.showsVerticalScrollIndicator = false
+        scrollView.showsHorizontalScrollIndicator = false
+
+        return scrollView
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
+        setSubViews()
+        setConstraints()
     }
+}
 
+private extension ViewController {
+    
+    func setSubViews() {
+        view.addSubview(detailScrollView)
+    }
+    
+    func setConstraints() {
+        NSLayoutConstraint.activate([
+            detailScrollView.topAnchor.constraint(equalTo: self.view.topAnchor),
+            detailScrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            detailScrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            detailScrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor)
+        ])
+    }
 }
