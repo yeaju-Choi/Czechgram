b8a8171fec103f3735ea6a089de344ee5bea3ac8
asqw887 <asqw887@gmail.com>
2022-07-11 17:08:29 +0900

    Feat: scrollView 설정

--- Czechgram/Czechgram/HomeViewController.swift
+++ Czechgram/Czechgram/HomeViewController.swift
@@ -11,6 +11,20 @@ final class HomeViewController: UIViewController {
 
     private var profileView = ProfileView()
 
+    private var scrollView: UIScrollView = {
+            let scrollView = UIScrollView()
+            scrollView.translatesAutoresizingMaskIntoConstraints = false
+            scrollView.backgroundColor = .white
+            scrollView.showsVerticalScrollIndicator = false
+            return scrollView
+        }()
+
+    private let contentView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .white
@@ -32,16 +46,35 @@ private extension HomeViewController {
     }
 
     func addSubviews() {
-        view.addSubview(profileView)
+
+        view.addSubview(scrollView)
+        scrollView.addSubview(contentView)
+        contentView.addSubview(profileView)
 
     }
 
     func setLayouts() {
+
+        NSLayoutConstraint.activate([
+            scrollView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
+            scrollView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            scrollView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            scrollView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor)
+        ])
+
+        NSLayoutConstraint.activate([
+            contentView.topAnchor.constraint(equalTo: scrollView.topAnchor),
+            contentView.leadingAnchor.constraint(equalTo: scrollView.leadingAnchor),
+            contentView.trailingAnchor.constraint(equalTo: scrollView.trailingAnchor),
+            contentView.bottomAnchor.constraint(equalTo: scrollView.bottomAnchor),
+            contentView.widthAnchor.constraint(equalTo: scrollView.widthAnchor)
+        ])
+
         profileView.translatesAutoresizingMaskIntoConstraints = false
         NSLayoutConstraint.activate([
-            profileView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor),
-            profileView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
-            profileView.widthAnchor.constraint(equalTo: self.view.widthAnchor),
+            profileView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            profileView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            profileView.widthAnchor.constraint(equalTo: contentView.widthAnchor),
             profileView.heightAnchor.constraint(equalToConstant: 220)
         ])
     }
