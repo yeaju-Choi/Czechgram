8f042f2acf107a8006046e2655d680aefe0a18e3
asqw887 <asqw887@gmail.com>
2022-07-12 17:52:37 +0900

    Feat: HTTPMethod 타입 구현

---
+++ Czechgram/Czechgram/Network/Endpoint.swift
@@ -0,0 +1,8 @@
+//
+//  Endpoint.swift
+//  Czechgram
+//
+//  Created by 최예주 on 2022/07/12.
+//
+
+import Foundation
---
+++ Czechgram/Czechgram/Network/Endpontable.swift
@@ -0,0 +1,8 @@
+//
+//  Endpontable.swift
+//  Czechgram
+//
+//  Created by 최예주 on 2022/07/12.
+//
+
+import Foundation
