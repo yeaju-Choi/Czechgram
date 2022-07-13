dbf4d6c4a273847bae26158d4732a006601699d5
asqw887 <asqw887@gmail.com>
2022-07-13 12:35:09 +0900

    Feat: urlRequest 생성 메소드구현

--- Czechgram/Czechgram/Network/Service/NetworkService.swift
+++ Czechgram/Czechgram/Network/Service/NetworkService.swift
@@ -8,25 +8,60 @@
 import Foundation
 
 struct NetworkService: NetworkServiceable {
-    
+
     static func request(endPoint: EndPoint, completion: @escaping CompletionHandler) {
-        var urlRequest = URLRequest(url: endPoint.base)
-        urlRequest.httpMethod = endPoint.httpMethod
-        urlRequest.setValue(endPoint.contentType, forHTTPHeaderField: endPoint.headerType)
-        urlRequest.httpBody = endPoint
-        
-        URLSession.shared.dataTask(with: urlRequest) { (data, response, error) in
-            guard let !error = error else {
+
+        let request = makeURLRequest(with: endPoint)
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            guard let httpResponse = response as? HTTPURLResponse else { return }
+
+            if let error = error {
                 completion(.failure(.transportError(error)))
                 return
             }
-            
-            guard let httpResponse = response as? HTTPURLResponse, (200...299).contains(httpResponse.statusCode) else {
+
+            guard (200...299).contains(httpResponse.statusCode) else {
                 completion(.failure(.serverError(statusCode: httpResponse.statusCode)))
                 return
             }
-            
+
+            guard let data = data else {
+                completion(.failure(.noData))
+                return
+            }
+
             completion(.success(data))
         }.resume()
+    }
+
+    static func makeURLRequest(with target: EndPoint) -> URLRequest {
+        guard var url = URL(string: target.base) else {
+            preconditionFailure("invalid base url")
+        }
+
+        if let path = target.path {
+            url = url.appendingPathComponent(path)
+        }
+
+        var components = URLComponents(string: "\(url)")
+
+        if let queryParam = target.parameter {
+            components?.queryItems = queryParam.map { URLQueryItem(name: $0, value: $1)
+            }
+        }
+
+        guard let validURL = components?.url else {
+            preconditionFailure("invalid url components")
+        }
+        var request = URLRequest(url: validURL)
+
+        if let header = target.contentType {
+            header.forEach { (key, value) in
+                request.addValue(key, forHTTPHeaderField: value)
+            }
+            request.httpMethod = target.httpMethod.value
+        }
+
+        return request
     }
 }
