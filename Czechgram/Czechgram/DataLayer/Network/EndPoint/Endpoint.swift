20234f6568eac5a172849dde66e83f665e44160c
asqw887 <asqw887@gmail.com>
2022-07-19 12:23:10 +0900

    Fix: requestToken 요청 쿼리아이템 수정 및 url 수정

--- Czechgram/Czechgram/DataLayer/Network/EndPoint/Endpoint.swift
+++ Czechgram/Czechgram/DataLayer/Network/EndPoint/Endpoint.swift
@@ -44,6 +44,9 @@ enum EndPoint: EndPontable {
 
     var contentType: [String: String]? {
         switch self {
+        case .requestToken(let code):
+            return ["Content-type": "application/x-www-form-urlencoded",
+                    "Accept": "application/json"]
         default:
             return ["Content-type": "application/json",
                     "Accept": "application/json"]
@@ -64,8 +67,9 @@ enum EndPoint: EndPontable {
         case .requestToken(let code):
             return [URLQueryItem(name: "client_id", value: "622682008886100"),
              URLQueryItem(name: "client_secret", value: "2b5c96cee7df4b0e8b5a8a291ed7d747"),
-             URLQueryItem(name: "code", value: "AQBx-hBsH3..."),
-             URLQueryItem(name: "grant_type", value: "\(code)")
+             URLQueryItem(name: "code", value: "\(code)"),
+             URLQueryItem(name: "grant_type", value: "authorization_code"),
+             URLQueryItem(name: "redirect_uri", value: "https://wnsxor1993.github.io/")
              ]
         }
     }
@@ -75,7 +79,9 @@ enum EndPoint: EndPontable {
         components.scheme = self.scheme
         components.host = self.host
         components.path = self.path ?? ""
+        if self.httpMethod == .get {
         components.queryItems = self.queryItems
+        }
 
         return components.url
     }
