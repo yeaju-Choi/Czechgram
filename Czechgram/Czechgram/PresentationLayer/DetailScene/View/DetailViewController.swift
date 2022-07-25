fb4ae41a8de8dcf824b51e444749b76a5f052f37
wnsxor1993 <wnsxor1993@naver.com>
2022-07-26 00:43:18 +0900

    refactor: @available 구문 추가 #22

--- Czechgram/Czechgram/PresentationLayer/DetailScene/View/DetailViewController.swift
+++ Czechgram/Czechgram/PresentationLayer/DetailScene/View/DetailViewController.swift
@@ -40,6 +40,7 @@ final class DetailViewController: UIViewController {
         super.init(nibName: nil, bundle: nil)
     }
 
+    @available (*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
